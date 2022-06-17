0f2e95b2ea18c552cb7e7f3c69ea53d7005bef86
kenji ishibashi <k.ishibashi0712@gmail.com>
2022-06-17 10:19:05 +0900

    add

---
+++ SingleGsheetDL/background.js
@@ -0,0 +1,20 @@
+function injectedFunction() {
+    if (location.href.match(/docs\.google\.com\/spreadsheets/) === null) {
+        alert('ここちゃうで。');
+    }
+    else {
+        var id = location.href.match(/\/d\/(.*)\//);
+        var sheetid = location.href.match(/gid=(\d+)/);
+        alert('ダウンロードしたろか？');
+        open('https://docs.google.com/spreadsheets/d/'+id[1]+'/export?format=xlsx&gid='+sheetid[1]);
+    }
+}
+
+chrome.action.onClicked.addListener((tab) => {
+    if(!tab.url.includes("chrome://")) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: injectedFunction
+        });
+    }
+});
